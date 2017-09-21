0fe5a58e8dedba0042bdea79615b93e076d1fc55
HugoMogollon <30709351+HugoMogollon@users.noreply.github.com>
2017-09-20 20:51:46 -0500

    Inicio integración dashboard.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.blank')
-
 @push('stylesheets')
-    <!-- Example -->
-    <!--<link href=" <link href="{{ asset("css/myFile.min.css") }}" rel="stylesheet">" rel="stylesheet">-->
+  <!-- Example -->
+  <!--<link href=" <link href="{{ asset("css/myFile.min.css") }}" rel="stylesheet">" rel="stylesheet">-->
 @endpush
-
 @section('main_container')
-    <!-- page content -->
-    <div class="right_col" role="main">
-    </div>
-    <!-- /page content -->
+  <!-- page content -->
+  <div class="right_col" role="main">
+  </div>
+  <!-- /page content -->
 @endsection
